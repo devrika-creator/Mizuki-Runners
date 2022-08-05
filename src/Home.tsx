93d5bad88da265ff1165bf0c6b571436cbae6a7d
eRikaNFT <nfterika@gmail.com>
2022-08-05 17:14:01 +0800

    Update Home.tsx

--- src/Home.tsx
+++ src/Home.tsx
@@ -611,9 +611,9 @@ const Home = (props: HomeProps) => {
                             {wallet && isActive &&
                               /*<h3>TOTAL MINTED : {itemsRedeemed} / {itemsAvailable}</h3>}*/
                               /* 87 is itemsRemaining 200 is itemsAvailable*/
-                              <h3>TOTAL MINTED : {903} / {1000}</h3>}
+                              <h3>TOTAL MINTED : {971} / {1000}</h3>}
                             {wallet && isActive && <BorderLinearProgress variant="determinate"
-                                                                         value={100 - (97 * 100 / 1000)}/>}
+                                                                         value={100 - (29 * 100 / 1000)}/>}
                             <br/>
                             <MintButtonContainer>
                                 {!isActive && !isEnded && candyMachine?.state.goLiveDate && (!isWLOnly || whitelistTokenBalance > 0) ? (
